ce442d9c7df435e7a7355754312b050c5dfd3582
David Waltermire <david.waltermire@nist.gov>
2022-10-28 14:03:17 -0400

    Adding missing unit test.

--- cli-core/src/main/java/gov/nist/secauto/oscal/tools/cli/core/commands/AbstractConvertSubcommand.java
+++ cli-core/src/main/java/gov/nist/secauto/oscal/tools/cli/core/commands/AbstractConvertSubcommand.java
@@ -186,7 +186,6 @@ public abstract class AbstractConvertSubcommand
     } else if (fromFormat.equals(toFormat)) {
       throw new IllegalArgumentException(String.format("Source and destination are the same format '%s'", toFormat));
     }
-
     convert(source, destination, fromFormat, toFormat, getLoadedClass(), context);
   }
 
