1d367cc693f867ba209b2f8d3054e3ecd23a5210
David Waltermire <david.waltermire@nist.gov>
2023-07-06 09:50:17 -0400

    Update to metaschema-java 0.11.0 (#155)
    * Adjustments to ensure working order with metaschema-java 0.11.0.
    
    * Migrated CLI framework code to the metaschema-java project.
    Moved metaschema commands to the metaschema-java project.
    
    * Flattened java modules into a single module.
    
    * Updated GHA workflows to address module flattening from prior commit.

---
+++ src/main/java/gov/nist/secauto/oscal/tools/cli/core/commands/AbstractRenderSubcommand.java
@@ -0,0 +1,136 @@
+/*
+ * Portions of this software was developed by employees of the National Institute
+ * of Standards and Technology (NIST), an agency of the Federal Government and is
+ * being made available as a public service. Pursuant to title 17 United States
+ * Code Section 105, works of NIST employees are not subject to copyright
+ * protection in the United States. This software may be subject to foreign
+ * copyright. Permission in the United States and in foreign countries, to the
+ * extent that NIST may hold copyright, to use, copy, modify, create derivative
+ * works, and distribute this software and its documentation without fee is hereby
+ * granted on a non-exclusive basis, provided that this notice and disclaimer
+ * of warranty appears in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED 'AS IS' WITHOUT ANY WARRANTY OF ANY KIND, EITHER
+ * EXPRESSED, IMPLIED, OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, ANY WARRANTY
+ * THAT THE SOFTWARE WILL CONFORM TO SPECIFICATIONS, ANY IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND FREEDOM FROM
+ * INFRINGEMENT, AND ANY WARRANTY THAT THE DOCUMENTATION WILL CONFORM TO THE
+ * SOFTWARE, OR ANY WARRANTY THAT THE SOFTWARE WILL BE ERROR FREE.  IN NO EVENT
+ * SHALL NIST BE LIABLE FOR ANY DAMAGES, INCLUDING, BUT NOT LIMITED TO, DIRECT,
+ * INDIRECT, SPECIAL OR CONSEQUENTIAL DAMAGES, ARISING OUT OF, RESULTING FROM,
+ * OR IN ANY WAY CONNECTED WITH THIS SOFTWARE, WHETHER OR NOT BASED UPON WARRANTY,
+ * CONTRACT, TORT, OR OTHERWISE, WHETHER OR NOT INJURY WAS SUSTAINED BY PERSONS OR
+ * PROPERTY OR OTHERWISE, AND WHETHER OR NOT LOSS WAS SUSTAINED FROM, OR AROSE OUT
+ * OF THE RESULTS OF, OR USE OF, THE SOFTWARE OR SERVICES PROVIDED HEREUNDER.
+ */
+
+package gov.nist.secauto.oscal.tools.cli.core.commands;
+
+import gov.nist.secauto.oscal.tools.cli.framework.CLIProcessor;
+import gov.nist.secauto.oscal.tools.cli.framework.ExitCode;
+import gov.nist.secauto.oscal.tools.cli.framework.ExitStatus;
+import gov.nist.secauto.oscal.tools.cli.framework.InvalidArgumentException;
+import gov.nist.secauto.oscal.tools.cli.framework.command.AbstractTerminalCommand;
+import gov.nist.secauto.oscal.tools.cli.framework.command.CommandContext;
+import gov.nist.secauto.oscal.tools.cli.framework.command.DefaultExtraArgument;
+import gov.nist.secauto.oscal.tools.cli.framework.command.ExtraArgument;
+
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.xml.transform.TransformerException;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+public abstract class AbstractRenderSubcommand
+    extends AbstractTerminalCommand {
+  private static final Logger LOGGER = LogManager.getLogger(AbstractRenderSubcommand.class);
+
+  private static final String COMMAND = "render";
+  private static final List<ExtraArgument> EXTRA_ARGUMENTS;
+
+  static {
+    List<ExtraArgument> args = new ArrayList<>(2);
+    args.add(new DefaultExtraArgument("source file", true));
+    args.add(new DefaultExtraArgument("destination file", false));
+    EXTRA_ARGUMENTS = Collections.unmodifiableList(args);
+  }
+
+  @Override
+  public String getName() {
+    return COMMAND;
+  }
+
+  @Override
+  public void gatherOptions(Options options) {
+    options
+        .addOption(Option.builder().longOpt("overwrite").desc("overwrite the destination if it exists").build());
+  }
+
+  @Override
+  @SuppressFBWarnings(value = "EI_EXPOSE_REP", justification = "unmodifiable collection and immutable item")
+  public List<ExtraArgument> getExtraArguments() {
+    return EXTRA_ARGUMENTS;
+  }
+
+  @Override
+  public void validateOptions(CLIProcessor processor, CommandContext context)
+      throws InvalidArgumentException {
+    List<String> extraArgs = context.getExtraArguments();
+    if (extraArgs.size() != 2) {
+      throw new InvalidArgumentException("Both a source and destination argument must be provided.");
+    }
+
+    File source = new File(extraArgs.get(0));
+    if (!source.exists()) {
+      throw new InvalidArgumentException("The provided source '" + source.getPath() + "' does not exist.");
+    }
+    if (!source.canRead()) {
+      throw new InvalidArgumentException("The provided source '" + source.getPath() + "' is not readable.");
+    }
+  }
+
+  @SuppressWarnings({
+    "PMD.OnlyOneReturn" // readability
+  })
+  @Override
+  public ExitStatus executeCommand(CLIProcessor processor, CommandContext context) {
+    List<String> extraArgs = context.getExtraArguments();
+    Path destination = resolvePathAgainstCWD(Paths.get(extraArgs.get(1))); // .toAbsolutePath();
+
+    if (Files.exists(destination)) {
+      if (!context.getCmdLine().hasOption("overwrite")) {
+        return ExitCode.FAIL.exitMessage("The provided destination '" + destination
+            + "' already exists and the --overwrite option was not provided.");
+      }
+      if (!Files.isWritable(destination)) {
+        return ExitCode.FAIL.exitMessage("The provided destination '" + destination + "' is not writable.");
+      }
+    }
+
+    Path input = resolvePathAgainstCWD(Paths.get(extraArgs.get(0)));
+    try {
+      performRender(input, destination);
+    } catch (IOException | TransformerException ex) {
+      return ExitCode.FAIL.exit().withThrowable(ex);
+    }
+
+    if (LOGGER.isInfoEnabled()) {
+      LOGGER.info("Generated HTML file: " + destination.toString());
+    }
+    return ExitCode.OK.exit();
+  }
+
+  protected abstract void performRender(Path input, Path result) throws IOException, TransformerException;
+}
---
+++ src/main/java/gov/nist/secauto/oscal/tools/cli/core/commands/profile/ResolveSubcommand.java
@@ -0,0 +1,293 @@
+/*
+ * Portions of this software was developed by employees of the National Institute
+ * of Standards and Technology (NIST), an agency of the Federal Government and is
+ * being made available as a public service. Pursuant to title 17 United States
+ * Code Section 105, works of NIST employees are not subject to copyright
+ * protection in the United States. This software may be subject to foreign
+ * copyright. Permission in the United States and in foreign countries, to the
+ * extent that NIST may hold copyright, to use, copy, modify, create derivative
+ * works, and distribute this software and its documentation without fee is hereby
+ * granted on a non-exclusive basis, provided that this notice and disclaimer
+ * of warranty appears in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED 'AS IS' WITHOUT ANY WARRANTY OF ANY KIND, EITHER
+ * EXPRESSED, IMPLIED, OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, ANY WARRANTY
+ * THAT THE SOFTWARE WILL CONFORM TO SPECIFICATIONS, ANY IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND FREEDOM FROM
+ * INFRINGEMENT, AND ANY WARRANTY THAT THE DOCUMENTATION WILL CONFORM TO THE
+ * SOFTWARE, OR ANY WARRANTY THAT THE SOFTWARE WILL BE ERROR FREE.  IN NO EVENT
+ * SHALL NIST BE LIABLE FOR ANY DAMAGES, INCLUDING, BUT NOT LIMITED TO, DIRECT,
+ * INDIRECT, SPECIAL OR CONSEQUENTIAL DAMAGES, ARISING OUT OF, RESULTING FROM,
+ * OR IN ANY WAY CONNECTED WITH THIS SOFTWARE, WHETHER OR NOT BASED UPON WARRANTY,
+ * CONTRACT, TORT, OR OTHERWISE, WHETHER OR NOT INJURY WAS SUSTAINED BY PERSONS OR
+ * PROPERTY OR OTHERWISE, AND WHETHER OR NOT LOSS WAS SUSTAINED FROM, OR AROSE OUT
+ * OF THE RESULTS OF, OR USE OF, THE SOFTWARE OR SERVICES PROVIDED HEREUNDER.
+ */
+
+package gov.nist.secauto.oscal.tools.cli.core.commands.profile;
+
+import gov.nist.secauto.metaschema.binding.io.DeserializationFeature;
+import gov.nist.secauto.metaschema.binding.io.Format;
+import gov.nist.secauto.metaschema.binding.io.IBoundLoader;
+import gov.nist.secauto.metaschema.binding.io.ISerializer;
+import gov.nist.secauto.metaschema.cli.processor.CLIProcessor.CallingContext;
+import gov.nist.secauto.metaschema.cli.processor.ExitCode;
+import gov.nist.secauto.metaschema.cli.processor.ExitStatus;
+import gov.nist.secauto.metaschema.cli.processor.InvalidArgumentException;
+import gov.nist.secauto.metaschema.cli.processor.OptionUtils;
+import gov.nist.secauto.metaschema.cli.processor.command.AbstractTerminalCommand;
+import gov.nist.secauto.metaschema.cli.processor.command.DefaultExtraArgument;
+import gov.nist.secauto.metaschema.cli.processor.command.ExtraArgument;
+import gov.nist.secauto.metaschema.model.common.metapath.DynamicContext;
+import gov.nist.secauto.metaschema.model.common.metapath.StaticContext;
+import gov.nist.secauto.metaschema.model.common.metapath.item.IDocumentNodeItem;
+import gov.nist.secauto.metaschema.model.common.util.CustomCollectors;
+import gov.nist.secauto.metaschema.model.common.util.ObjectUtils;
+import gov.nist.secauto.oscal.lib.OscalBindingContext;
+import gov.nist.secauto.oscal.lib.model.Catalog;
+import gov.nist.secauto.oscal.lib.model.Profile;
+import gov.nist.secauto.oscal.lib.profile.resolver.ProfileResolutionException;
+import gov.nist.secauto.oscal.lib.profile.resolver.ProfileResolver;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Locale;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+
+public class ResolveSubcommand
+    extends AbstractTerminalCommand {
+
+  @NonNull
+  private static final String COMMAND = "resolve";
+  @NonNull
+  private static final List<ExtraArgument> EXTRA_ARGUMENTS = ObjectUtils.notNull(List.of(
+      new DefaultExtraArgument("file to resolve", true),
+      new DefaultExtraArgument("destination file", false)));
+  @NonNull
+  private static final Option AS_OPTION = ObjectUtils.notNull(
+      Option.builder()
+          .longOpt("as")
+          .hasArg()
+          .argName("FORMAT")
+          .desc("source format: xml, json, or yaml")
+          .build());
+  @NonNull
+  private static final Option TO_OPTION = ObjectUtils.notNull(
+      Option.builder()
+          .longOpt("to")
+          .required()
+          .hasArg().argName("FORMAT")
+          .desc("convert to format: xml, json, or yaml")
+          .build());
+
+  @Override
+  public String getName() {
+    return COMMAND;
+  }
+
+  @Override
+  public String getDescription() {
+    return "Resolve the specified OSCAL Profile";
+  }
+
+  @Override
+  public Collection<? extends Option> gatherOptions() {
+    return List.of(
+        AS_OPTION,
+        TO_OPTION);
+  }
+
+  @Override
+  public List<ExtraArgument> getExtraArguments() {
+    return EXTRA_ARGUMENTS;
+  }
+
+  @SuppressWarnings({
+      "PMD.CyclomaticComplexity", "PMD.CognitiveComplexity" // reasonable
+  })
+  @Override
+  public void validateOptions(CallingContext callingContext, CommandLine cmdLine) throws InvalidArgumentException {
+    if (cmdLine.hasOption(AS_OPTION)) {
+      try {
+        String toFormatText = cmdLine.getOptionValue(AS_OPTION);
+        Format.valueOf(toFormatText.toUpperCase(Locale.ROOT));
+      } catch (IllegalArgumentException ex) {
+        InvalidArgumentException newEx = new InvalidArgumentException(
+            String.format("Invalid '%s' argument. The format must be one of: %s.",
+                OptionUtils.toArgument(AS_OPTION),
+                Arrays.asList(Format.values()).stream()
+                    .map(format -> format.name())
+                    .collect(CustomCollectors.joiningWithOxfordComma("and"))));
+        newEx.setOption(AS_OPTION);
+        newEx.addSuppressed(ex);
+        throw newEx;
+      }
+    }
+
+    if (cmdLine.hasOption(TO_OPTION)) {
+      try {
+        String toFormatText = cmdLine.getOptionValue(TO_OPTION);
+        Format.valueOf(toFormatText.toUpperCase(Locale.ROOT));
+      } catch (IllegalArgumentException ex) {
+        InvalidArgumentException newEx = new InvalidArgumentException("Invalid '--to' argument. The format must be one of: "
+            + Arrays.asList(Format.values()).stream()
+                .map(format -> format.name())
+                .collect(CustomCollectors.joiningWithOxfordComma("and")));
+        newEx.setOption(AS_OPTION);
+        newEx.addSuppressed(ex);
+        throw newEx;
+      }
+    }
+
+    List<String> extraArgs = cmdLine.getArgList();
+    if (extraArgs.isEmpty()) {
+      throw new InvalidArgumentException("The source to resolve must be provided.");
+    }
+
+    File source = new File(extraArgs.get(0));
+    if (!source.exists()) {
+      throw new InvalidArgumentException("The provided source '" + source.getPath() + "' does not exist.");
+    }
+    if (!source.canRead()) {
+      throw new InvalidArgumentException("The provided source '" + source.getPath() + "' is not readable.");
+    }
+  }
+
+  @SuppressWarnings({
+      "PMD.OnlyOneReturn" // readability
+  })
+  @Override
+  public ExitStatus executeCommand(CallingContext callingContext, CommandLine cmdLine) {
+    List<String> extraArgs = cmdLine.getArgList();
+    Path source = resolvePathAgainstCWD(Paths.get(extraArgs.get(0)));
+
+    IBoundLoader loader = OscalBindingContext.instance().newBoundLoader();
+    loader.disableFeature(DeserializationFeature.DESERIALIZE_VALIDATE_CONSTRAINTS);
+
+    Format asFormat;
+    // attempt to determine the format
+    if (cmdLine.hasOption(AS_OPTION)) {
+      try {
+        String asFormatText = cmdLine.getOptionValue(AS_OPTION);
+        asFormat = Format.valueOf(asFormatText.toUpperCase(Locale.ROOT));
+      } catch (IllegalArgumentException ex) {
+        return ExitCode.FAIL
+            .exitMessage("Invalid '--as' argument. The format must be one of: " + Arrays.stream(Format.values())
+                .map(format -> format.name())
+                .collect(CustomCollectors.joiningWithOxfordComma("or")));
+      }
+    } else {
+      // attempt to determine the format
+      try {
+        asFormat = loader.detectFormat(source);
+      } catch (FileNotFoundException ex) {
+        // this case was already checked for
+        return ExitCode.INPUT_ERROR.exitMessage("The provided source file '" + source + "' does not exist.");
+      } catch (IOException ex) {
+        return ExitCode.FAIL.exit().withThrowable(ex);
+      } catch (IllegalArgumentException ex) {
+        return ExitCode.FAIL.exitMessage(
+            "Source file has unrecognizable format. Use '--as' to specify the format. The format must be one of: "
+                + Arrays.stream(Format.values())
+                    .map(format -> format.name())
+                    .collect(CustomCollectors.joiningWithOxfordComma("or")));
+      }
+    }
+
+    source = source.toAbsolutePath();
+
+    Format toFormat;
+    if (cmdLine.hasOption(TO_OPTION)) {
+      String toFormatText = cmdLine.getOptionValue(TO_OPTION);
+      toFormat = Format.valueOf(toFormatText.toUpperCase(Locale.ROOT));
+    } else {
+      toFormat = asFormat;
+    }
+
+    Path destination = null;
+    if (extraArgs.size() == 2) {
+      destination = Paths.get(extraArgs.get(1)).toAbsolutePath();
+    }
+
+    if (destination != null) {
+      if (Files.exists(destination)) {
+        if (!cmdLine.hasOption("overwrite")) {
+          return ExitCode.FAIL.exitMessage("The provided destination '" + destination
+              + "' already exists and the --overwrite option was not provided.");
+        }
+        if (!Files.isWritable(destination)) {
+          return ExitCode.FAIL.exitMessage("The provided destination '" + destination + "' is not writable.");
+        }
+      } else {
+        Path parent = destination.getParent();
+        if (parent != null) {
+          try {
+            Files.createDirectories(parent);
+          } catch (IOException ex) {
+            return ExitCode.INVALID_TARGET.exit().withThrowable(ex);
+          }
+        }
+      }
+    }
+
+    IDocumentNodeItem document;
+    try {
+      document = loader.loadAsNodeItem(asFormat, loader.toInputSource(source.toUri()));
+    } catch (IOException ex) {
+      return ExitCode.INPUT_ERROR.exit().withThrowable(ex);
+    }
+    Object object = document.getValue();
+    if (object instanceof Catalog) {
+      // this is a catalog
+      return ExitCode.FAIL.exitMessage("The target file is already a catalog");
+    } else if (object instanceof Profile) {
+      // this is a profile
+      StaticContext staticContext = new StaticContext();
+      URI sourceUri = source.toUri();
+      staticContext.setBaseUri(sourceUri);
+      DynamicContext dynamicContext = staticContext.newDynamicContext();
+      dynamicContext.setDocumentLoader(loader);
+      ProfileResolver resolver = new ProfileResolver();
+      resolver.setDynamicContext(dynamicContext);
+
+      IDocumentNodeItem resolvedProfile;
+      try {
+        resolvedProfile = resolver.resolve(document);
+      } catch (IOException | ProfileResolutionException ex) {
+        return ExitCode.FAIL
+            .exitMessage(
+                String.format("Unable to resolve profile '%s'. %s", document.getDocumentUri(), ex.getMessage()))
+            .withThrowable(ex);
+      }
+
+      // DefaultConstraintValidator validator = new DefaultConstraintValidator(dynamicContext);
+      // ((IBoundXdmNodeItem)resolvedProfile).validate(validator);
+      // validator.finalizeValidation();
+
+      ISerializer<Catalog> serializer
+          = OscalBindingContext.instance().newSerializer(toFormat, Catalog.class);
+      try {
+        if (destination == null) {
+          serializer.serialize((Catalog) resolvedProfile.getValue(), System.out);
+        } else {
+          serializer.serialize((Catalog) resolvedProfile.getValue(), destination);
+        }
+      } catch (IOException ex) {
+        return ExitCode.FAIL.exit().withThrowable(ex);
+      }
+    }
+    return ExitCode.OK.exit();
+  }
+}
