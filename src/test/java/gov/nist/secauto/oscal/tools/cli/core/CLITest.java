4b6b20570d624b834e218e5932e8789c7e36bdae
A.J. Stein <alexander.stein@nist.gov>
2023-11-30 14:26:47 -0500

    Add CLI-level integration tests for common commands (#211)
    * Add integration tests for #178.
    
    * Use only paths, not URLs
    
    Adjust method to load files PR per @david-waltermire-nist's rec.
    
    * Parameterize tests and fix bad profiles for #178.
    
    * Add junit-jupiter-params to POM test scope
    
    Per discussion with Dave and re-reviewing the tutorial section on steps
    for installation I had missed, add that dependency to the POM for #178.
    
    Also, per his recommendation, add it to the test scope and do not set an
    explicit version number because the core JUnit5 version is handled by the
    support POM for oss-maven project shared across the different Java
    projects.

--- src/test/java/gov/nist/secauto/oscal/tools/cli/core/CLITest.java
+++ src/test/java/gov/nist/secauto/oscal/tools/cli/core/CLITest.java
@@ -30,21 +30,26 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import gov.nist.secauto.metaschema.binding.io.Format;
 import gov.nist.secauto.metaschema.cli.processor.ExitCode;
 import gov.nist.secauto.metaschema.cli.processor.ExitStatus;
+import gov.nist.secauto.oscal.lib.profile.resolver.ProfileResolutionException;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
 
 public class CLITest {
-
-  @Test
-  void testVersionInfo() {
-    String[] args = { "--version" };
-    evaluateResult(CLI.runCli(args), ExitCode.OK);
-  }
-
   void evaluateResult(@NonNull ExitStatus status, @NonNull ExitCode expectedCode) {
     status.generateMessage(true);
     assertAll(
@@ -60,5 +65,68 @@ public class CLITest {
     assertAll(
         () -> assertEquals(expectedCode, status.getExitCode(), "exit code mismatch"),
         () -> assertEquals(thrownClass, thrown.getClass(), "expected Throwable mismatch"));
+  }
+
+  private static Stream<Arguments> providesValues() {
+    final String[] commands = { "ap", "ar", "catalog", "component-definition", "profile", "poam", "ssp" };
+    final Map<Format, List<Format>> formatEntries = Map.of(
+        Format.XML, Arrays.asList(Format.JSON, Format.YAML),
+        Format.JSON, Arrays.asList(Format.XML, Format.JSON),
+        Format.YAML, Arrays.asList(Format.XML, Format.JSON));
+    List<Arguments> values = new ArrayList<>();
+
+    values.add(Arguments.of(new String[] { "--version" }, ExitCode.OK, null));
+    // TODO: Test all data formats once usnistgov/oscal-cli#216 fix merged.
+    Path path = Paths.get("src/test/resources/cli/example_profile_invalid" + Format.XML.getDefaultExtension());
+    values.add(
+        Arguments.of(new String[] { "profile", "resolve", "--to=" + Format.XML.name().toLowerCase(), path.toString() },
+            ExitCode.PROCESSING_ERROR, ProfileResolutionException.class));
+
+    for (String cmd : commands) {
+      values.add(Arguments.of(new String[] { cmd, "validate", "-h" }, ExitCode.OK, null));
+      // TODO: Update when usnistgov/oscal-cli#210 fix merged.
+      values.add(Arguments.of(new String[] { cmd, "convert", "-h" }, ExitCode.INVALID_COMMAND, null));
+
+      for (Format format : Format.values()) {
+        path = Paths.get("src/test/resources/cli/example_" + cmd + "_invalid" + format.getDefaultExtension());
+        values.add(Arguments.of(new String[] { cmd, "validate", path.toString() }, ExitCode.FAIL, null));
+        path = Paths.get("src/test/resources/cli/example_" + cmd + "_valid" + format.getDefaultExtension());
+        values.add(Arguments.of(new String[] { cmd, "validate", path.toString() }, ExitCode.OK, null));
+        path = Paths.get("src/test/resources/cli/example_profile_valid" + format.getDefaultExtension());
+        List<Format> targetFormats = formatEntries.get(format);
+        for (Format targetFormat : targetFormats) {
+          path = Paths.get("src/test/resources/cli/example_" + cmd + "_valid" + format.getDefaultExtension());
+          String outputPath = path.toString().replace(format.getDefaultExtension(),
+              "_converted" + targetFormat.getDefaultExtension());
+          values.add(Arguments.of(new String[] { cmd, "convert", "--to=" + targetFormat.name().toLowerCase(),
+              path.toString(), outputPath, "--overwrite" }, ExitCode.OK, null));
+          // TODO: Update when usnistgov/oscal#217 fix merged.
+          path = Paths.get("src/test/resources/cli/example_" + cmd + "_invalid" + format.getDefaultExtension());
+          outputPath = path.toString().replace(format.getDefaultExtension(),
+              "_converted" + targetFormat.getDefaultExtension());
+          values.add(Arguments.of(new String[] { cmd, "convert", "--to=" + targetFormat.name().toLowerCase(),
+              path.toString(), outputPath, "--overwrite" }, ExitCode.OK, null));
+        }
+        if (cmd == "profile") {
+          path = Paths.get("src/test/resources/cli/example_profile_valid" + format.getDefaultExtension());
+          values
+              .add(Arguments.of(new String[] { cmd, "resolve", "--to=" + format.name().toLowerCase(), path.toString() },
+                  ExitCode.OK, null));
+        }
+      }
+    }
+
+    return values.stream();
+  }
+
+  @ParameterizedTest
+  @MethodSource("providesValues")
+  void testAllSubCommands(@NonNull String[] args, @NonNull ExitCode expectedExitCode,
+      Class<? extends Throwable> expectedThrownClass) {
+    if (expectedThrownClass == null) {
+      evaluateResult(CLI.runCli(args), expectedExitCode);
+    } else {
+      evaluateResult(CLI.runCli(args), expectedExitCode, expectedThrownClass);
+    }
   }
 }
